83cd96a41c1d8784f3c7b22eecfde4d711c58ec1
Wetitpig <winsto003@hotmail.com>
2017-06-23 11:31:21 +0800

    Use NOT logical operator rather than the -1 multiplier

--- noisecancel.c
+++ noisecancel.c
@@ -7,8 +7,8 @@
 
 int main()
 {
-	short *buffer;
-	int err, i, j;
+	short *buffer = (short*)malloc(44100 * sizeof(short));;
+	int err, i, j = sizeof(buffer);
 	snd_pcm_t *inhandle, *outhandle;
 	
 	snd_pcm_open(&inhandle, "default", SND_PCM_STREAM_CAPTURE, 0);
@@ -17,14 +17,11 @@ int main()
 	snd_pcm_set_params(inhandle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 1, 44100, 1, 0);
 	snd_pcm_set_params(outhandle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 1, 44100, 1, 0);
 
-	buffer = (short*)malloc(44100 * sizeof(short));
-	j = sizeof(buffer);
-
 	while (likely(1)) {
 		if (snd_pcm_readi(inhandle, buffer, j) < 0)
 			snd_pcm_prepare(inhandle);
-		for (i = 0; i < j; ++i)
-			buffer[i] *= -1;
+		for(i = 0; i < j; ++i)
+			~buffer[i];
 		if ((err = snd_pcm_writei(outhandle, buffer, j)) < 0) {
 			if (snd_pcm_recover(outhandle, err, 1) >= 0)
 				snd_pcm_writei(outhandle, buffer, j);
